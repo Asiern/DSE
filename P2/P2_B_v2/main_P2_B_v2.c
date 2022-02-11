d983cfcaca33dd7964ae20744ba34cb8871cfa0b
asiern <asnulorri@gmail.com>
2022-02-11 11:34:30 +0100

    P2_B_v2

---
+++ P2/P2_B_v2/main_P2_B_v2.c
@@ -0,0 +1,36 @@
+/*
+Cronometro mediante leds.
+Control de timer 1 por encuesta.
+Se utilizan los siguientes leds:
+-------- // COMPLETAD!!!!!!
+
+Autorxs:
+Fecha:
+*/
+
+#include "p24HJ256GP610A.h"
+
+#include "oscilator.h"
+#include "GPIO.h"
+#include "timers.h"
+#include "commons.h"
+
+int main()
+{
+
+	while (PORTDbits.RD7)
+		Nop();
+
+	inic_oscilator(); // Seleccion e inicializacion del reloj: 40 MHz
+
+	inic_leds(); // Inicializacio�n leds: sentido y valor inicial.
+
+	inic_crono(); // Inicializacion variables cronometro.
+
+	inic_Timer7(); // Inicializacion T7 con un periodo de 10 milisegundos.
+
+	while (1)
+		cronometro();
+
+	return (0);
+}
